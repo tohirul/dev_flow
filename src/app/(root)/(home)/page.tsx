0fe7990f29beaaca55ce46e4cbc6e834d50b06c6
tohirul <tohirul.islam164@gmail.com>
2025-02-19 13:16:32 +0600

    [Fixed] Hydration Error

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,13 +1,13 @@
 import Link from 'next/link';
 
-// import { UserButton } from '@clerk/nextjs';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <section>
       <Link href='/projects'>See Projects</Link>
       <h1>Home</h1>
-      {/* <UserButton afterSwitchSessionUrl='/' /> */}
+      <UserButton afterSwitchSessionUrl='/' />
     </section>
   );
 }
